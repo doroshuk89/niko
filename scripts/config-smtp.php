d8bde82d913fdd41379adc046cd694a0a6f872d4
Atlas <doroshuk33@yandex.by>
2020-04-16 14:44:48 +0300

    fixBug

--- scripts/config-smtp.php
+++ scripts/config-smtp.php
@@ -11,6 +11,6 @@ $smtp = array(
     "secure" => 'ssl', // Тип шифрования. Например ssl или tls
     "mail_title" => 'Заголовок вашего письма!', // Заголовок письма
     "mail_name" => 'Nkn-Мебель', // Имя отправителя
-    "addAddress" => 'doroshuk33@yandex.ru' //Адресс получателя
+    "addAddress" => 'info@nkn-mebel.by' //Адрес получателя
 );
 
